d5d1f679c5d73145dc211d205908093d95a57d13
Anton Okunkov <ryamova37@gmail.com>
2020-10-16 01:02:05 +0300

    Создание вспомогательных методов и классов

---
+++ addressbook-web-tests/addressbook-web-tests/Scuare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class Scuare : Figure
+    {
+        private int size;
+        
+
+        public Scuare (int size)
+        {
+              this.size = size;
+        }
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -7,9 +7,36 @@ namespace addressbook_web_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodScuare()
         {
+            Scuare s1 = new Scuare (5);
+            Scuare s2 = new Scuare (10);
+            Scuare s3 = s1;
+        
+            Assert.AreEqual(s1.Size,5);
+            Assert.AreEqual(s2.Size,10);
+            Assert.AreEqual(s3.Size,5);
 
+            s3.Size = 15;
+            Assert.AreEqual(s1.Size,15);
+
+            s2.Colored = true;
+        }
+        [TestMethod]
+        public void TestMethodCircle()
+        {
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+            Assert.AreEqual(s1.Radius, 15);
+
+            s2.Colored = true;
         }
     }
 }
