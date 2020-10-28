2d96d148bfb2e2594fa7eee964dd1af7a2fe4d9e
Anton Okunkov <ryamova37@gmail.com>
2020-10-28 11:08:41 +0300

    Создание базового класса для тестовых классов

--- addressbook-web-tests/addressbook-web-tests/ContactRemovalTest.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactRemovalTest.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 
-namespace addressbook_web_tests
+namespace addressbook_web_tests 
 {
     [TestFixture]
-    public class ContactRemovalTests
+    public class ContactRemovalTests : TestBase
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -39,102 +38,18 @@ namespace addressbook_web_tests
             Assert.AreEqual("", verificationErrors.ToString());
         }
 
+
         [Test]
         public void ContactRemovalTest()
         {
             GoToHomePage();
-            Login(new AccountData ("admin","secret"));
+            Login(new AccountData("admin", "secret"));
             GoToContactsPage();
             SelectContact("1");
-            RemoveContact();
-            ReturnToContactsPage();
+            //RemoveContact();
+            //ReturnToContactPage();
 
         }
 
-        private void ReturnToContactsPage()
-        {
-            driver.FindElement(By.LinkText("home")).Click();
-        }
-
-        private void RemoveContact()
-        {
-            Assert.IsTrue(Regex.IsMatch(CloseAlertAndGetItsText(), "^Delete 1 addresses[\\s\\S]$"));
-        }
-
-        private void SelectContact(string index)
-        {
-            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();           
-            acceptNextAlert = true;
-            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
-        }
-
-        private void GoToContactsPage()
-        {
-            driver.FindElement(By.LinkText("home")).Click();
-        }
-
-        private void Login(AccountData account)
-        {
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(account.Username);
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-        }
-
-        private void GoToHomePage()
-        {
-            driver.Navigate().GoToUrl("http://localhost/addressbook/");
-        }
-
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
     }
 }
--- addressbook-web-tests/addressbook-web-tests/TestBase.cs
+++ addressbook-web-tests/addressbook-web-tests/TestBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -15,7 +16,9 @@ namespace addressbook_web_tests
     {
         protected IWebDriver driver;
         private StringBuilder verificationErrors;
-        protected string baseURL;        
+        protected string baseURL;
+
+        protected bool acceptNextAlert;
 
         [SetUp]
         public void SetupTest()
@@ -40,7 +43,7 @@ namespace addressbook_web_tests
         }
         protected void GoToHomePage()
         {
-            driver.Navigate().GoToUrl("http://localhost/addressbook/group.php");
+            driver.Navigate().GoToUrl("http://localhost/addressbook");
         }
         protected void Login(AccountData account)
         {
@@ -90,6 +93,96 @@ namespace addressbook_web_tests
         {
 
             driver.FindElement(By.Name("submit")).Click();
+        }
+        protected void GoToContactsPage()
+        {
+            driver.FindElement(By.LinkText("home")).Click();
+        }
+
+        protected void GoToContactCreationPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        protected void InitNewContactCreation()
+        {
+
+            driver.FindElement(By.Name("firstname")).Click();
+        }
+        protected void FillContactForm(ContactData contact)
+        {
+
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("middlename")).Click();
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            driver.FindElement(By.Name("title")).Click();
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(contact.Title);
+            driver.FindElement(By.Name("address")).Click();
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contact.Address);
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contact.Mobile);
+            driver.FindElement(By.Name("email")).Click();
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(contact.Email);
+            driver.FindElement(By.Name("bday")).Click();
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(contact.Bday);
+            driver.FindElement(By.Name("bday")).Click();
+            driver.FindElement(By.Name("bmonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(contact.Bmonth);
+            driver.FindElement(By.Name("bmonth")).Click();
+            driver.FindElement(By.Name("byear")).Click();
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(contact.Byear);
+        }
+        protected void SubmitContactCreation()
+        {
+
+            driver.FindElement(By.XPath("(//input[@name='submit'])[2]")).Click();
+        }
+        protected void ReturnToContactPage()
+        {
+
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+        protected void SelectContact(string index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+          
+            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
+        }
+        protected void RemoveContact()
+        {
+            acceptNextAlert = true;
+            Assert.IsTrue(Regex.IsMatch(CloseAlertAndGetItsText(), "^Delete 1 addresses[\\s\\S]$"));
+        }
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
